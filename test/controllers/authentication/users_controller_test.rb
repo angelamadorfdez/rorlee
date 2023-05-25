1c1fe709f166e9b6a39d53f00fa3d457d799a5c7
angelamadorfdez <angel.amadorfdez@gmail.com>
2023-05-25 16:31:00 +0200

    sign up tests

---
+++ test/controllers/authentication/users_controller_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class Authentication::UserControllerTest < ActionDispatch::IntegrationTest
+  
+  def test_get_new_user
+    get new_authentication_user_path	
+    assert_response :success
+  end
+
+  def test_create_user
+    assert_difference('User.count') do    
+      post authentication_users_path, params: { user: { email: 'jose@ingenio.xyz', username: 'jose97', password: 'testme' } }
+    end
+  end
+
+end
