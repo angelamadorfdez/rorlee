9bada6a9fb9b6a97fcf2c750025b67a66b38599f
angelamadorfdez <angel.amadorfdez@gmail.com>
2023-05-25 16:34:35 +0200

    use urls in test

--- test/controllers/authentication/users_controller_test.rb
+++ test/controllers/authentication/users_controller_test.rb
@@ -3,13 +3,13 @@ require "test_helper"
 class Authentication::UserControllerTest < ActionDispatch::IntegrationTest
   
   def test_get_new_user
-    get new_authentication_user_path	
+    get new_authentication_user_url
     assert_response :success
   end
 
   def test_create_user
     assert_difference('User.count') do    
-      post authentication_users_path, params: { user: { email: 'jose@ingenio.xyz', username: 'jose97', password: 'testme' } }
+      post authentication_users_url, params: { user: { email: 'jose@ingenio.xyz', username: 'jose97', password: 'testme' } }
     end
   end
 
